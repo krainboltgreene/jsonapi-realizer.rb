27f140f73a4c6c25ff99b9dc53a6d9b34cd91280
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2015-08-19 15:31:20 -0500

    Better to have less files, ruby config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,26 @@
 require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
-
 require "pry"
 require "rspec"
 require "blankgem"
+
+RSpec.configure do |let|
+  let.before("suite") do
+    CodeClimate::TestReporter.start
+  end
+
+  # Exit the spec after the first failure
+  let.fail_fast = true
+
+  # Only run a specific file, using the ENV variable
+  # Example: FILE=spec/blankgem/version_spec.rb bundle exec rake spec
+  let.pattern = ENV["FILE"]
+
+  # Show the slowest examples in the suite
+  let.profile_examples = true
+
+  # Colorize the output
+  let.color = true
+
+  # Output as a document string
+  let.default_formatter = "doc"
+end
