78b131e0e2bbf774bddb5521cc34047a7e0609f7
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-31 01:43:46 -0700

    Give helpful warning message

--- lib/jsonapi/realizer/error/invalid_accept_header.rb
+++ lib/jsonapi/realizer/error/invalid_accept_header.rb
@@ -2,7 +2,14 @@ module JSONAPI
   module Realizer
     class Error
       class InvalidAcceptHeader < Error
-
+        def initialize(given:, wanted:)
+          @given = given
+          @wanted = wanted
+        end
+        
+        def message
+          "HTTP Header recieved is #{given}, but expected #{wanted}"
+        end
       end
     end
   end
