2f555204990f7309e70564f806d2108087feb23b
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-31 01:48:50 -0700

    Now this exception can give context

--- lib/jsonapi/realizer/error/invalid_content_type_header.rb
+++ lib/jsonapi/realizer/error/invalid_content_type_header.rb
@@ -2,7 +2,14 @@ module JSONAPI
   module Realizer
     class Error
       class InvalidContentTypeHeader < Error
-
+        def initialize(given:, wanted:)
+          @given = given
+          @wanted = wanted
+        end
+        
+        def message
+          "HTTP Content-Type Header recieved is #{@given}, but expected #{@wanted}"
+        end
       end
     end
   end
