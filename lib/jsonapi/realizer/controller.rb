62705bf464c5a94a9a12cfbf6d8341c13f6db7e5
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2023-09-18 12:41:05 -0700

    Correcting some typos and expanding the helper functionality

--- lib/jsonapi/realizer/controller.rb
+++ lib/jsonapi/realizer/controller.rb
@@ -3,9 +3,24 @@
 module JSONAPI
   module Realizer
     module Controller
+      private def reject_missing_accept_type_header
+        return if request.body.empty?
+        return if request.headers.key?("HTTP_ACCEPT")
+
+        raise(JSONAPI::Realizer.configuration.default_missing_accept_type_exception)
+      end
+
+      private def reject_invalid_accept_type_header
+        reject_missing_accept_type_header
+
+        return if request.headers.fetch("HTTP_ACCEPT").include?(JSONAPI::MEDIA_TYPE)
+
+        raise(JSONAPI::Realizer.configuration.default_invalid_accept_type_exception)
+      end
+
       private def reject_missing_content_type_header
         return if request.body.empty?
-        return if request.headers.property?("Content-Type")
+        return if request.headers.key?("Content-Type")
 
         raise(JSONAPI::Realizer.configuration.default_missing_content_type_exception)
       end
@@ -20,8 +35,8 @@ module JSONAPI
 
       private def reject_missing_root_property
         return if request.parameters.key?("body")
-        return if request.paremters.key?("errors")
-        return if request.paremters.key?("meta")
+        return if request.parameters.key?("errors")
+        return if request.parameters.key?("meta")
 
         raise(Error::MissingRootProperty)
       end
--- lib/jsonapi/realizer/version.rb
+++ lib/jsonapi/realizer/version.rb
@@ -2,6 +2,6 @@
 
 module JSONAPI
   module Realizer
-    VERSION = "6.0.0"
+    VERSION = "6.1.0"
   end
 end
