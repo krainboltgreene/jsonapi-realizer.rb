882511a087758f2b8f315f6ecb3af371bc031a46
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2023-09-19 10:34:59 -0700

    Removing methods that aren't valuable and fixing a bug with the controller helpers

--- lib/jsonapi/realizer/controller.rb
+++ lib/jsonapi/realizer/controller.rb
@@ -4,7 +4,7 @@ module JSONAPI
   module Realizer
     module Controller
       private def reject_missing_accept_type_header
-        return if request.body.empty?
+        return if request.body.read.empty?
         return if request.headers.key?("HTTP_ACCEPT")
 
         raise(JSONAPI::Realizer.configuration.default_missing_accept_type_exception)
@@ -19,7 +19,7 @@ module JSONAPI
       end
 
       private def reject_missing_content_type_header
-        return if request.body.empty?
+        return if request.body.read.empty?
         return if request.headers.key?("Content-Type")
 
         raise(JSONAPI::Realizer.configuration.default_missing_content_type_exception)
@@ -31,41 +31,6 @@ module JSONAPI
         return if request.headers.fetch("Content-Type").include?(JSONAPI::MEDIA_TYPE)
 
         raise(JSONAPI::Realizer.configuration.default_invalid_content_type_exception)
-      end
-
-      private def reject_missing_root_property
-        return if request.parameters.key?("body")
-        return if request.parameters.key?("errors")
-        return if request.parameters.key?("meta")
-
-        raise(Error::MissingRootProperty)
-      end
-
-      private def reject_invalid_root_property
-        reject_missing_root_property
-
-        return unless request.parameters.key?("data") && (request.parameters.fetch("data").is_a?(Hash) || request.parameters.fetch("data").is_a?(Array))
-        return unless request.parameters.key?("errors") && request.parameters.fetch("errors").is_a?(Array)
-
-        raise(Error::InvalidRootProperty)
-      end
-
-      private def reject_missing_type_property
-        reject_invalid_root_property
-
-        return if request.parameters.fetch("data").is_a?(Hash) && request.parameters.fetch("data").key?("type")
-        return if request.parameters.fetch("data").is_a?(Array) && request.parameters.fetch("data").all? { |data| data.key?("type") }
-
-        raise(Error::MissingDataTypeProperty)
-      end
-
-      private def reject_invalid_type_property
-        reject_missing_type_property
-
-        return if request.parameters.fetch("data").is_a?(Hash) && request.parameters.fetch("data").fetch("type").is_a?(String) && request.parameters.fetch("data").fetch("type").present?
-        return if request.parameters.fetch("data").is_a?(Array) && request.parameters.fetch("data").map { |data| data.fetch("type") }.all? { |type| type.is_a?(String) && type.present? }
-
-        raise(Error::InvalidDataTypeProperty)
       end
     end
   end
